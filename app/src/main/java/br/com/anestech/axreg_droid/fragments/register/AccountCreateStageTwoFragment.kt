a5e14252263c2c97513e8ebaffee66dc8a78a740
Vinicius Paulino <vinydos3@gmail.com>
2018-07-11 15:31:45 -0300

    continuar

--- app/src/main/java/br/com/anestech/axreg_droid/fragments/register/AccountCreateStageTwoFragment.kt
+++ app/src/main/java/br/com/anestech/axreg_droid/fragments/register/AccountCreateStageTwoFragment.kt
@@ -1,29 +1,27 @@
 package br.com.anestech.axreg_droid.fragments.register
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.Toast
-import br.com.anestech.axcalc.database.DbHelper
 import br.com.anestech.axreg_droid.R
-import br.com.anestech.axreg_droid.adapter.ListaCrmAdapter
-import br.com.anestech.axreg_droid.database.DbCrmAnesthetist
 import br.com.anestech.axreg_droid.database.DbState
-import br.com.anestech.axreg_droid.models.CrmAnesthetist
-import br.com.anestech.axreg_droid.models.State
 import br.com.anestech.axreg_droid.validator.DefaultValidation
+import kotlinx.android.synthetic.main.fragment_account_create_stage_three.*
 import kotlinx.android.synthetic.main.fragment_account_create_stage_two.*
-import kotlinx.android.synthetic.main.fragment_account_create_stage_two.view.*
-import java.util.*
 
-class AccountCreateStageTwoFragment : Fragment() {
+class AccountCreateStageTwoFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
-    lateinit var crmAdapter: ListaCrmAdapter
-    private  var listCrms: MutableList<CrmAnesthetist>? = null
+
+    //lateinit var crmAdapter: ListaCrmAdapter
+    private var listState = arrayOf("PA","TO", "SP")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -32,44 +30,66 @@ class AccountCreateStageTwoFragment : Fragment() {
 
 
 
-        val listState = DbState().getAcronymState()
-        Log.e("lista de estado", "Lista de estador $listState")
-        val adapter =  ArrayAdapter(context, R.layout.support_simple_spinner_dropdown_item, listState)
-
-
-        view.spinner.adapter = adapter
 
         return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    private fun configSpinner() {
+        //spinner = this.spinner
 
-        btnAdd.setOnClickListener{
+        spinner!!.onItemSelectedListener = this
 
-            //testar campo null
-            if (DefaultValidation(edt_register_crm).isValid()){
-                Toast.makeText(context, "Adicionar CRM", Toast.LENGTH_LONG).show()
+        //Criando ArrayAdapter com Lista
+//        val arrayAdapter = ArrayAdapter(context, android.R.layout.simple_spinner_item, listState)
+//        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-                val stateSP : State = State(2, "Sao Paulo", "SP")
-                val stateRJ : State = State(3, "Rio de Janeiro", "RJ")
+        //Criando ArrayAdapter com Resource
+        val arrayAdapter = ArrayAdapter.createFromResource(context, R.array.list_state, android.R.layout.simple_spinner_dropdown_item)
 
-                val crmRj : CrmAnesthetist = CrmAnesthetist(uuid = UUID.randomUUID().toString(), updateAt = Calendar.getInstance().time, active = true, crm = edt_register_crm.text.toString(), state = stateRJ)
-                val crmSp : CrmAnesthetist = CrmAnesthetist(uuid = UUID.randomUUID().toString(), updateAt = Calendar.getInstance().time, active = true, crm = "485296", state = stateSP)
-
-
-                DbHelper.save(crmRj)
-
-                Log.e("tamanho da lista", listCrms?.size.toString())
-
-
-                listCrms = DbCrmAnesthetist().getListCrm()
-                crmAdapter = ListaCrmAdapter(context = context!!, list = listCrms!!)
-
-                lista_cmr_recyclerview.adapter = crmAdapter
-
-            }
-        }
+        spinner?.adapter = arrayAdapter
     }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Log.e("ItemSelecionado", "Acronym State Selecionado $" + listState[position])
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configSpinner()
+    }
+
+
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//
+//        btnAdd.setOnClickListener{
+//
+//            //testar campo null
+//            if (DefaultValidation(edt_register_crm).isValid()){
+//                Toast.makeText(context, "Adicionar CRM", Toast.LENGTH_LONG).show()
+//
+//                val stateSP : State = State(2, "Sao Paulo", "SP")
+//                val stateRJ : State = State(3, "Rio de Janeiro", "RJ")
+//
+//                val crmRj : CrmAnesthetist = CrmAnesthetist(uuid = UUID.randomUUID().toString(), updateAt = Calendar.getInstance().time, active = true, crm = edt_register_crm.text.toString(), state = stateRJ)
+//                val crmSp : CrmAnesthetist = CrmAnesthetist(uuid = UUID.randomUUID().toString(), updateAt = Calendar.getInstance().time, active = true, crm = "485296", state = stateSP)
+//
+//                DbHelper.save(crmRj)
+//
+//                Log.e("tamanho da lista", listCrms?.size.toString())
+//
+//
+//                listCrms = DbCrmAnesthetist().getListCrm()
+//                crmAdapter = ListaCrmAdapter(context = context!!, list = listCrms!!)
+//
+//                lista_cmr_recyclerview.adapter = crmAdapter
+//
+//            }
+//        }
+//    }
 
 }
--- app/src/main/java/br/com/anestech/axreg_droid/models/CrmAnesthetist.kt
+++ app/src/main/java/br/com/anestech/axreg_droid/models/CrmAnesthetist.kt
@@ -1,12 +1,15 @@
 package br.com.anestech.axreg_droid.models
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 import java.util.*
 
 /**
  * Created by vinicius on 06/06/18.
  */
 open class CrmAnesthetist (
+        @PrimaryKey
+        var id: Long? = null,
         var uuid: String = "",
         var updateAt:Date? = null,
         var active: Boolean = true,
